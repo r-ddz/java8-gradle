580c9ec4357a56985d777218e31ddda288d13a31
rui <740660485@qq.com>
2025-12-09 20:37:52 +0800

    StreamDemo List => Map

--- ddz-demo/src/main/java/com/ddz/demo/stream/StreamDemo.java
+++ ddz-demo/src/main/java/com/ddz/demo/stream/StreamDemo.java
@@ -1,14 +1,15 @@
 package com.ddz.demo.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamDemo {
 
     public static void main(String[] args) {
         // 根据条件过滤数据，找到第一个满足条件的数据
         findFirst();
+        // List => Map
+        list2map();
     }
 
     /**
@@ -31,6 +32,26 @@ public class StreamDemo {
         } else {
             System.out.println("没有找到");
         }
+    }
+
+    /**
+     * List => Map
+     */
+    private static void list2map() {
+        String[] arr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "2"};
+        List<String> list = new ArrayList<>(Arrays.asList(arr));
+//        Map<String, String> taskIdMap = webErpToDoEntity.stream()
+//                .collect(Collectors.toMap(WebErpToDoEntity::getProcessInstanceId, WebErpToDoEntity::getTaskId, (k1, k2) -> k1));
+        Map<String, String> map = list.stream()
+                .collect(Collectors.toMap(
+                        (String k) -> "key_" + k,
+                        (String v) -> "value_" + v,
+                        // 如果有重复的 key，则保留第一个 value
+                        (k1, k2) -> k1
+                ));
+        System.out.println("================================= 开始 =================================");
+        System.out.println(map);
+        System.out.println("================================= 结束 =================================");
     }
 
 
