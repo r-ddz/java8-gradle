f890b7db76facb2f7b7d6718b88542709a855393
张瑞 <740660485@qq.com>
2026-02-10 19:05:09 +0800

    钉钉机器人工具

---
+++ ddz-demo-dingtalk/src/main/java/com/ddz/demo/dingtalk/DingTalkUtil.java
@@ -0,0 +1,131 @@
+package com.ddz.demo.dingtalk;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.util.Arrays;
+
+@Slf4j
+public class DingTalkUtil {
+
+    private static final String SERVER_URL = "https://oapi.dingtalk.com/robot/send";
+    private static final String CHARSET_NAME = "UTF-8";
+    private static final String SIGN_ALGORITHMS = "HmacSHA256";
+
+    private final String secret;
+    private final String accessToken;
+    private final OapiRobotSendRequest request;
+    private final OapiRobotSendRequest.At requestAt;
+
+    public static DingTalkUtil create(String secret, String accessToken) {
+        return new DingTalkUtil(secret, accessToken);
+    }
+
+    private DingTalkUtil(String secret, String accessToken) {
+        this.secret = secret;
+        this.accessToken = accessToken;
+        this.request = new OapiRobotSendRequest();
+        this.requestAt = new OapiRobotSendRequest.At();
+    }
+
+    public void send() {
+        try {
+            Long timestamp = System.currentTimeMillis();
+            String stringToSign = timestamp + "\n" + this.secret;
+            Mac mac = Mac.getInstance(SIGN_ALGORITHMS);
+            mac.init(new SecretKeySpec(this.secret.getBytes(CHARSET_NAME), SIGN_ALGORITHMS));
+            byte[] signData = mac.doFinal(stringToSign.getBytes(CHARSET_NAME));
+            String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)),CHARSET_NAME);
+
+            //sign字段和timestamp字段必须拼接到请求URL上，否则会出现 310000 的错误信息
+            DingTalkClient client = new DefaultDingTalkClient(SERVER_URL + "?sign="+sign+"&timestamp="+timestamp);
+            this.request.setAt(this.requestAt);
+            OapiRobotSendResponse rsp = client.execute(this.request, this.accessToken);
+            if (!rsp.isSuccess()) {
+                log.error("发送钉钉消息失败 errcode：{} errmsg: {}", rsp.getErrcode(), rsp.getErrmsg());
+                log.error("发送钉钉消息失败：{}", rsp.getBody());
+            }
+        } catch (Exception e) {
+            log.error("发送钉钉消息失败", e);
+            log.error("出现异常", e);
+        }
+    }
+
+    public DingTalkUtil markdown(String title, String text) {
+        this.request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle(title);
+        markdown.setText(text);
+        this.request.setMarkdown(markdown);
+        return this;
+    }
+
+    public DingTalkUtil markdown(String title, String template, Object... params) {
+        String text = StrUtil.format(template, params);
+        return markdown(title, text);
+    }
+
+    public DingTalkUtil text(String content) {
+        this.request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent(content);
+        this.request.setText(text);
+        return this;
+    }
+
+    public DingTalkUtil text(String template, Object... params) {
+        String content = StrUtil.format(template, params);
+        return text(content);
+    }
+
+    public DingTalkUtil link(String title, String messageUrl, String text) {
+        this.request.setMsgtype("link");
+        OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
+        link.setTitle(title);
+        link.setText(text);
+        link.setMessageUrl(messageUrl);
+        this.request.setLink(link);
+        return this;
+    }
+
+    public DingTalkUtil atAll() {
+        this.requestAt.setIsAtAll(true);
+        return this;
+    }
+
+    public DingTalkUtil atUser(String... userIds) {
+        if (CollUtil.isEmpty(requestAt.getAtUserIds())) {
+            this.requestAt.setAtUserIds(new java.util.ArrayList<String>());
+        }
+        this.requestAt.getAtUserIds().addAll(Arrays.asList(userIds));
+        return this;
+    }
+
+    public DingTalkUtil setAtUserIds(String... userIds) {
+        this.requestAt.setAtUserIds(Arrays.asList(userIds));
+        return this;
+    }
+
+    public DingTalkUtil atMobile(String... mobiles) {
+        if (CollUtil.isEmpty(requestAt.getAtMobiles())) {
+            this.requestAt.setAtMobiles(new java.util.ArrayList<String>());
+        }
+        this.requestAt.getAtMobiles().addAll(Arrays.asList(mobiles));
+        return this;
+    }
+
+    public DingTalkUtil setAtMobiles(String... mobiles) {
+        this.requestAt.setAtMobiles(Arrays.asList(mobiles));
+        return this;
+    }
+
+}
---
+++ ddz-demo-dingtalk/src/test/java/com/ddz/demo/dingtalk/DingTalkTests.java
@@ -0,0 +1,65 @@
+package com.ddz.demo.dingtalk;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class DingTalkTests {
+
+    @Test
+    public void test1() {
+
+        String secret = "SECeeaf158057195cb3c3e1007422ef9f5fa38193546aa7c10799c0bbfd759a4b32";
+        String accessToken = "e7488672d2f5cf7aee06e4d3daa811e11c66335a74a92938ec8c7fb882e67003";
+
+        String title = "测试 title";
+        // 定义模板，markdown场景下，需要在内容里加入 @对象
+        String template =   "**首行加粗字体** <br>" +
+                            "环境： **{}** <br>" +
+                            "参数1：**{}** <br>" +
+                            "参数2：**{}** <br> @ZIY00220103 @all";
+
+        DingTalkUtil.create(secret, accessToken)
+                .markdown(title, template, "dev", "测试12344", "测试666888")
+//                .atAll()
+                .atUser("ZIY00220103")
+                .send();
+    }
+
+    @Test
+    public void test2() {
+        String secret = "SECeeaf158057195cb3c3e1007422ef9f5fa38193546aa7c10799c0bbfd759a4b32";
+        String accessToken = "e7488672d2f5cf7aee06e4d3daa811e11c66335a74a92938ec8c7fb882e67003";
+
+        // 内部群才能精确 @个人
+        DingTalkUtil.create(secret, accessToken)
+                .text("测试text121231224234")
+//                .atAll()
+                .atUser("ZIY00220103")
+                .send();
+    }
+
+    @Test
+    public void test3() {
+
+        String secret = "SEC225a1a9f8ecfb6169a086eb8c4f49b768e7b4defbbfd92f447327a21cf767eea";
+        String accessToken = "8a75234acfe48e03a927b80c5c1ff3e05709dc67ca9f7a68ea18e2a8cd34a6b0";
+
+        // 非内部群只能 @所有人，不支持精确 @个人
+        DingTalkUtil.create(secret, accessToken)
+                .text("测试text121231224234")
+                .atAll()
+                .send();
+    }
+
+    @BeforeAll
+    public static void before(){
+        System.out.println("================================= 测试开始 =================================");
+    }
+
+    @AfterAll
+    public static void after(){
+        System.out.println("================================= 测试结束 =================================");
+    }
+
+}
