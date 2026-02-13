289de447f4a481c27741b6e952922ae4dff7e195
张瑞 <740660485@qq.com>
2026-02-13 15:13:18 +0800

    钉钉机器人工具

--- ddz-demo-dingtalk/src/main/java/com/ddz/demo/dingtalk/helper/DingTalkHelperFactory.java
+++ ddz-demo-dingtalk/src/main/java/com/ddz/demo/dingtalk/helper/DingTalkHelperFactory.java
@@ -3,12 +3,19 @@ package com.ddz.demo.dingtalk.helper;
 import cn.hutool.core.util.StrUtil;
 import com.ddz.demo.dingtalk.config.DingTalkConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+//@Slf4j
 @Component
 public class DingTalkHelperFactory {
+
+    // 工具包日志手写，可以避免一定需要引入注解的依赖（效果与@Slf4j一致）
+    private static final Logger logger = LoggerFactory.getLogger(DingTalkHelperFactory.class);
+
+
 
     private static final DingTalkHelper NULL_HELPER = new NullDingTalkHelper();
 
@@ -17,7 +24,7 @@ public class DingTalkHelperFactory {
 
     public DingTalkHelper create(String robotKey) {
         if (StrUtil.isBlank(robotKey)) {
-            log.error("【钉钉】查找机器人入参不能为空");
+            logger.error("【钉钉】查找机器人入参不能为空");
             return NULL_HELPER;
         }
         String secret = null;
@@ -30,7 +37,7 @@ public class DingTalkHelperFactory {
             }
         }
         if (StrUtil.isBlank(secret) || StrUtil.isBlank(accessToken)) {
-            log.error("【钉钉】找不到钉钉机器人配置: {}", robotKey);
+            logger.error("【钉钉】找不到钉钉机器人配置: {}", robotKey);
             return NULL_HELPER;
         }
         return new DefaultDingTalkHelper(secret, accessToken);
